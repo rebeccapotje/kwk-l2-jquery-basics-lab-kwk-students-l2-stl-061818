7eb15216c7f1614cf26475389a6008fec3d5dcfe
rebeccapotje <rebecca.potje@gmail.com>
2018-06-25 13:52:57 +0000

    automatically backed up by learn

--- js/calc.js
+++ js/calc.js
@@ -1,1 +1,23 @@
 //add your code here
+
+let numOne = $('#number1')
+let numTwo = $('#number2')
+
+let operator = $('#operation')
+
+function validate(operator, numOne, numTwo) {
+  if (operator == '+' || operator == '-' || operator == '*' || operator == '/') 
+  { if(isNaN(numOne) || numOne=== '' || isNaN(numTwo) || numTwo=== '' ) 
+  { $('#result').text('Sorry, one of those is not a valid number! ');} else {answer(operator, numOne, numTwo)};}
+  else{$('#result').html("Sorry, that is not a valid operator");
+  
+}
+ 
+ }
+
+
+
+//function myCalculator(a,b) {
+  //if userInput = '+'
+    //numOne + numTwo
+//}
